98509360d2cf7593aab537346ecee99a348fc5ff
Saketh <sakethmynampati@billericak12.com>
2021-04-29 13:50:57 -0400

    new

--- src/base/tower/HanoiTower.java
+++ src/base/tower/HanoiTower.java
@@ -1,5 +1,7 @@
 package base.tower;
 
+import java.util.Date;
+
 public class HanoiTower {
     private Peg peg1;
     private Peg peg2;
@@ -10,6 +12,7 @@ public class HanoiTower {
     {
         // TODO implement constructor
         // Peg 1 is the starting Peg
+        numDiscs = num;
         peg1 = new Peg(1, num);
         peg2 = new Peg(2, num);
         peg3 = new Peg(3, num);
@@ -25,14 +28,22 @@ public class HanoiTower {
 
     private void moveTower(Peg startPeg, Peg endPeg, Peg extraPeg, int numtoMove)
     {
-        // TODO move discs(number input) from the start peg to the end peg
-
-
+        if (numtoMove == 1) { //base case
+            // TODO move discs(number input) from the start peg to the end peg
+            startPeg.moveTopDisc(endPeg);
+        } else {
+            moveTower(startPeg, extraPeg, endPeg, numtoMove - 1);
+            startPeg.moveTopDisc(endPeg);
+            moveTower(extraPeg, endPeg, startPeg, numtoMove - 1);
+        }
     }
     public static void main(String[] args) {
         // TODO Auto-generated method stub
+        Date i = new Date();
         HanoiTower ht = new HanoiTower(5);
         ht.solveTower();
+        Date f = new Date();
+        System.out.println("Elapsed: " + (f.getTime() - i.getTime())/1000.);
     }
 
 }
